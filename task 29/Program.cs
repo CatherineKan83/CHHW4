1566dafe34ae6f1ffe68d82b2c5e6a38e183f032
Catherine <kankatya@bk.ru>
2022-08-23 03:24:00 +0500

    updated

--- task 29/Program.cs
+++ task 29/Program.cs
@@ -18,8 +18,7 @@ void FillArray(int[,] arr)
 {
     for(int i = 0; i<arr.GetLength(0); i++)
     {
-        for(
-            int j =0; j<arr.GetLength(1); j++)
+        for(int j =0; j<arr.GetLength(1); j++)
         {
             arr[i,j] = new Random().Next(1,100);
         }
