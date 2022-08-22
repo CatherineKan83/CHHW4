99a3078888850e9982015147afab3a730254639e
Catherine <kankatya@bk.ru>
2022-08-23 02:25:41 +0500

    Added task 27

---
+++ task 27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while(1<=num)
+{
+    sum = sum + (num%10);
+    num = num/10;        
+}
+Console.Write($"Сумма цифр, составляющих число, равна {sum}.");
+
+
