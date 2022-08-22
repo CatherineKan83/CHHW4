9262f134a01e3f078e14b12b6b382df603c2abbc
Catherine <kankatya@bk.ru>
2022-08-23 01:01:02 +0500

    Added task 25

---
+++ task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+double A = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Для возведения {A} в степень, введние натуральное число: ");
+double B = Convert.ToInt32(Console.ReadLine());
+if(B>0)
+{
+    Console.WriteLine($"{A}^{B} = {Math.Pow(A, B)}");
+}
+else Console.WriteLine("Ошибка ввода");
