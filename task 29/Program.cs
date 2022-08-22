a285d839c5da3585db5ee3788973c1f279e60a7f
Catherine <kankatya@bk.ru>
2022-08-23 03:06:35 +0500

    Added task 29

---
+++ task 29/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintArray(int[,] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i<arr.GetLength(0); i++)
+    {
+        for(int j =0; j<arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]}");
+            if(j<arr.GetLength(1)-1)
+            {
+                Console.Write(",");
+            }
+        }
+    Console.WriteLine("]");
+    }
+}
+void FillArray(int[,] arr)
+{
+    for(int i = 0; i<arr.GetLength(0); i++)
+    {
+        for(
+            int j =0; j<arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(1,100);
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,]array = new int[1, 8];
+FillArray(array);
+PrintArray(array);
